42832fcc4388f624545bbc97d2689c3da5243040
Mario Zupan <mario@zupzup.org>
2023-11-29 20:44:47 +0100

    start srv

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,17 @@
 use leptos::*;
-use rust_crdt_example::App;
 
+#[cfg(feature = "server")]
+mod server;
+
+#[cfg(feature = "server")]
+#[tokio::main]
+async fn main() {
+    println!("Hello, Server!");
+    server::run().await
+}
+
+#[cfg(not(feature = "server"))]
 fn main() {
-    println!("Hello, world!");
+    println!("Hello, client!");
     mount_to_body(|| view! { <App />})
 }
