03ec3a550149e4aa65091f9026786ade8f28f784
Mario Zupan <mario@zupzup.org>
2023-12-01 17:23:39 +0100

    server baseline

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,20 +1,44 @@
+use futures_util::{future, StreamExt, TryStreamExt};
+use log::info;
+use std::{env, io::Error};
+use tokio::net::{TcpListener, TcpStream};
+
 #[tokio::main]
-async fn main() {
-    println!("Hello, Server!");
-    run().await;
+async fn main() -> Result<(), Error> {
+    let _ = env_logger::try_init();
+    let addr = env::args()
+        .nth(1)
+        .unwrap_or_else(|| "127.0.0.1:3000".to_string());
+
+    // Create the event loop and TCP listener we'll accept connections on.
+    let try_socket = TcpListener::bind(&addr).await;
+    let listener = try_socket.expect("Failed to bind");
+    info!("Listening on: {}", addr);
+
+    while let Ok((stream, _)) = listener.accept().await {
+        tokio::spawn(accept_connection(stream));
+    }
+
+    Ok(())
 }
 
-pub async fn run() {
-    // let app = Router::new().route("ws", get(ws_handler));
+async fn accept_connection(stream: TcpStream) {
+    let addr = stream
+        .peer_addr()
+        .expect("connected streams should have a peer address");
+    info!("Peer address: {}", addr);
 
-    // let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
-    //     .await
-    //     .unwrap();
-    // // tracing::debug!("listening on {}", listener.local_addr().unwrap());
-    // axum::serve(
-    //     listener,
-    //     app.into_make_service_with_connect_info::<SocketAddr>(),
-    // )
-    // .await
-    // .unwrap();
+    let ws_stream = tokio_tungstenite::accept_async(stream)
+        .await
+        .expect("Error during the websocket handshake occurred");
+
+    info!("New WebSocket connection: {}", addr);
+
+    let (write, read) = ws_stream.split();
+
+    // We should not forward messages other than text or binary.
+    read.try_filter(|msg| future::ready(msg.is_text() || msg.is_binary()))
+        .forward(write)
+        .await
+        .expect("Failed to forward messages")
 }
